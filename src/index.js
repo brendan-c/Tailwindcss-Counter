292339f8d7c8d5da65e378c0369b2844ac4e2f1a
Brendan C <30474072+brendan-c@users.noreply.github.com>
2019-10-17 22:09:05 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,35 @@
+function decrement(e) {
+  const btn = e.target.parentNode.parentElement.querySelector(
+    'button[data-action="decrement"]'
+  );
+  const target = btn.nextElementSibling;
+  let value = Number(target.value);
+  value--;
+  target.value = value;
+}
+
+function increment(e) {
+  const btn = e.target.parentNode.parentElement.querySelector(
+    'button[data-action="decrement"]'
+  );
+  const target = btn.nextElementSibling;
+  let value = Number(target.value);
+  value++;
+  target.value = value;
+}
+
+const decrementButtons = document.querySelectorAll(
+  `button[data-action="decrement"]`
+);
+
+const incrementButtons = document.querySelectorAll(
+  `button[data-action="increment"]`
+);
+
+decrementButtons.forEach(btn => {
+  btn.addEventListener("click", decrement);
+});
+
+incrementButtons.forEach(btn => {
+  btn.addEventListener("click", increment);
+});
